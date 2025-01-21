f89bdc424b47e9fa3cc3352a45f656daf1b7bd00
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2025-01-21 20:58:10 +0000

    fix

--- lib/taskdir/taskdir.go
+++ lib/taskdir/taskdir.go
@@ -28,23 +28,45 @@ func NewTaskDir(dir string, initDir func(info TaskInfo) error) *TaskDir {
 	}
 }
 
+func (t *TaskDir) idToDir(id string) string {
+	return filepath.Join(t.dir, id)
+}
+
+func (t *TaskDir) initDir(info TaskInfo) error {
+	if t.InitDir != nil {
+		return t.InitDir(info)
+	}
+	return nil
+}
+
 func (t *TaskDir) NewTask() (info TaskInfo, err error) {
 	info.Id = uuid.New().String()
-	info.Dir = filepath.Join(t.dir, info.Id)
+	info.Dir = t.idToDir(info.Id)
 	err = os.MkdirAll(info.Dir, 0755)
 	if err != nil {
 		return
 	}
-	if t.InitDir != nil {
-		err = t.InitDir(info)
-	}
+	err = t.initDir(info)
 	return
 }
 
 func (t *TaskDir) RemoveTask(taskId string) error {
-	return os.RemoveAll(filepath.Join(t.dir, taskId))
+	return os.RemoveAll(t.idToDir(taskId))
 }
 
 func (t *TaskDir) JoinTask(taskId string) string {
-	return filepath.Join(t.dir, taskId)
+	path := t.idToDir(taskId)
+	_, err := os.Stat(path)
+	if err == nil {
+		return path
+	}
+	if !os.IsNotExist(err) {
+		// TODO: FATAL
+		return path
+	}
+
+	if info, err := t.NewTask(); err == nil {
+		return info.Dir
+	}
+	return path
 }
